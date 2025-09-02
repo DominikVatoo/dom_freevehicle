10c892c082a8c3bd8fd9833f2fb4c42fc9a9875b
DominikVatoo <92688475+DominikVatoo@users.noreply.github.com>
2025-09-02 14:07:08 +0200

    Update main.lua
    TriggerEvent("ws_notify", …) change to xPlayer.showNotification("…")

--- dom_freeveh/server/main.lua
+++ dom_freeveh/server/main.lua
@@ -1,20 +1,20 @@
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-local database = LoadResourceFile(GetCurrentResourceName(), "database.json") or "{}";
-database = database and database ~= "" and json.decode(database) or {};
+local database = LoadResourceFile(GetCurrentResourceName(), "database.json") or "{}"
+database = database and database ~= "" and json.decode(database) or {}
 
-local freeCar = 'i30npriordesign';
+local freeCar = 'i30npriordesign'
 
 RegisterCommand('freecar', function(source)
-  local xPlayer = ESX.GetPlayerFromId(source);
-  
-  if database[xPlayer.identifier] == true then
-    return TriggerEvent("ws_notify", "error", "Free-Car", "Du hast bereits ein gratis Auto erhalten!", 5000);
-  end
+    local xPlayer = ESX.GetPlayerFromId(source)
 
-  database[xPlayer.identifier] = true;
-  SaveResourceFile(GetCurrentResourceName(), "database.json", json.encode(database), -1);
+    if database[xPlayer.identifier] == true then
+        return xPlayer.showNotification("Du hast bereits ein gratis Auto erhalten!")
+    end
 
-  ExecuteCommand('_givecar ' .. xPlayer.source .. ' ' .. freeCar);
-  return TriggerEvent("ws_notify", "success", "Free-Car", "Dominik hat gegönnt viel Spaß mit deinem Auto!", 5000);
-end);
+    database[xPlayer.identifier] = true
+    SaveResourceFile(GetCurrentResourceName(), "database.json", json.encode(database), -1)
+
+    ExecuteCommand('_givecar ' .. xPlayer.source .. ' ' .. freeCar)
+    xPlayer.showNotification("Dominik hat gegönnt! Viel Spaß mit deinem Auto!")
+end)
