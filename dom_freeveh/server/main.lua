a223113725aa67539365f552fb63fe27507b3468
DominikVatoo <92688475+DominikVatoo@users.noreply.github.com>
2025-09-02 14:11:45 +0200

    Update main.lua
    Race condition prevented using isSaving + Citizen.Wait.

--- dom_freeveh/server/main.lua
+++ dom_freeveh/server/main.lua
@@ -5,15 +5,25 @@ database = database and database ~= "" and json.decode(database) or {}
 
 local freeCar = 'i30npriordesign'
 
+local isSaving = false
+
 RegisterCommand('freecar', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
+    if not xPlayer then return end
 
     if database[xPlayer.identifier] == true then
         return xPlayer.showNotification("Du hast bereits ein gratis Auto erhalten!")
     end
 
     database[xPlayer.identifier] = true
+
+    while isSaving do
+        Citizen.Wait(50) 
+    end
+
+    isSaving = true
     SaveResourceFile(GetCurrentResourceName(), "database.json", json.encode(database), -1)
+    isSaving = false
 
     ExecuteCommand('_givecar ' .. xPlayer.source .. ' ' .. freeCar)
     xPlayer.showNotification("Dominik hat gegönnt! Viel Spaß mit deinem Auto!")
